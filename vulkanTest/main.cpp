7b91e342fdc1b332d695bc677a632d2ee74e2e97
YuriTu <tuqiangfe@gmail.com>
2022-11-03 16:18:24 +0800

    feat: update pysical

--- vulkanTest/main.cpp
+++ vulkanTest/main.cpp
@@ -4,9 +4,21 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG 
+    const bool enableValidationLayer = false;
+#else 
+    const bool enableValidationLayer = true;
+#endif
+
 
 class HelloTriangleApplication {
 public:
@@ -20,6 +32,7 @@ public:
 private:
     GLFWwindow* window;
     VkInstance instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
     void initWindow() {
         glfwInit();
@@ -29,6 +42,11 @@ private:
     }
 
     void createInstance() {
+        if (enableValidationLayer && !checkValidationLayerSupport()) {
+            throw std::runtime_error("validation layers requestd, but not available!");
+        }
+
+
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "Hello Triangle";
@@ -41,27 +59,26 @@ private:
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
 
-        uint32_t glfwExtensionCount = 0;
-        const char** glfwExtensions;
+        auto extensions = getRequiredExtensions();
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
 
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-        createInfo.enabledExtensionCount = glfwExtensionCount;
-        createInfo.ppEnabledExtensionNames = glfwExtensions;
-        createInfo.enabledLayerCount = 0;
-
-        // uint32_t extensionCount = 0;
-        // vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-        // std::vector<VkExtensionProperties> extensions(extensionCount);
-
-        // vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
-        // std::cout << "avilable extension:\n";
-
-        // for (const auto& extension : extensions) {
-        //     std::cout << '\t' << extension.extensionName << '\n';
-        // }
+        if (enableValidationLayer) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
         
+
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+        std::vector<VkExtensionProperties> vkExtensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, vkExtensions.data());
+        std::cout << "avilable extension:\n";
+        for (const auto& extension : vkExtensions) {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
         
 
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
@@ -69,8 +86,72 @@ private:
         }
     }
 
+    bool checkValidationLayerSupport() {
+        uint32_t layerCount;
+        // 如果给null enumer会赋值他所支持的layer的数量
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayer(layerCount);
+
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayer.data());
+
+        // 检查上面要求的validation layer在不在available layer中
+        for (const char* layerName : validationLayers) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayer) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData
+    ) {
+        std::cerr << "Validation layer:" << pCallbackData->pMessage << std::endl;
+
+        return VK_FALSE;
+    }
+
+    std::vector<const char*> getRequiredExtensions() {
+        uint32_t extensionCount = 0;
+        const char** glfwExtensions;
+        // 获得windows情况下的必须extension 
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&extensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + extensionCount);
+        // 并带上debug的ext
+        if (enableValidationLayer) {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        return extensions;
+    }
+
     void initVulkan() {
         createInstance();
+        // setupDebugMessage()
+        pickPhysicalDevice();
+    }
+
+    void pickPhysicalDevice() {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+        if (deviceCount == 0) {
+            std::runtime_error("fail to find GPUs with Vulkan support!");
+        }
     }
 
     void mainLoop() {
