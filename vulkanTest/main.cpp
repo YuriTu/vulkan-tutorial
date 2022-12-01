debb485b065970b9a95220ca6b5bc7e960019c72
YuriTu <tuqiangfe@gmail.com>
2022-12-01 15:59:33 +0800

    feat: update alignas

--- vulkanTest/main.cpp
+++ vulkanTest/main.cpp
@@ -110,10 +110,9 @@ struct Vertex {
 };
 // 所有的数据一定要以16 对齐
 struct UniformBufferObject {
-    glm::vec2 foo;
     alignas(16)glm::mat4 model;
-    glm::mat4 view;
-    glm::mat4 proj;
+    alignas(16)glm::mat4 view;
+    alignas(16)glm::mat4 proj;
     
 };
 const std::vector<Vertex> vertices = {
